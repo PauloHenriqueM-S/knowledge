6b97fc8b4d7dea98e9e3b5bd68f6364024013f0b
Paulo Henrique <phttm0501@gmail.com>
2023-11-17 20:16:44 -0400

    Validator

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -1,6 +1,24 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-  const save = (req, res) => {
-    res.send('user save')
+  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+
+  const encryptPassword = password => {
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+  }
+
+  const save = async (req, res) => {
+    const user = { ...req.body }
+    if (req.params.id) user.id = req.params.id
+
+    try {
+      existsOrError(user.name, 'Nome não informado')
+      existsOrError(user.email, 'E-mail não informado')
+      existsOrError(user.password, 'Senha não informada')
+      existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
+      equalsOrError(user.password, user.confirmPassword, 'Senhas não conferem')
+    }
   }
 
   return { save }

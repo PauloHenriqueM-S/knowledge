78a713a8959f1b10a86597e4e10900e135a5e94b
Paulo Henrique <phttm0501@gmail.com>
2023-11-18 22:03:39 -0400

    User and routes att

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,11 +1,12 @@
 const app = require('express')()
 const consign = require('consign')
-const db = require("")
+const db = require("./config/db")
 
 app.db = db
 
 consign()
   .then('./config/middlewares.js')
+  .then('./api/validator.js')
   .then('./api')
   .then('./config/routes.js')
   .into(app)

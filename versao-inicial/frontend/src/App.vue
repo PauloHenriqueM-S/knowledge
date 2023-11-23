e08875ee033a8a3231b4985af82b6b110a63ffcb
Paulo Henrique <phttm0501@gmail.com>
2023-11-23 03:21:29 -0400

    header and menu functions with store

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <Header />
+  <div id="app" :class="{ 'hide-menu': !isMenuVisible }">
+    <Header title="Cod3r - Base de Conhecimento" :hideToggle="false" />
     <Menu />
     <Content />
     <Footer />
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import Header from "./components/template/Header.vue";
 import Menu from "./components/template/Menu.vue";
 import Content from "./components/template/Content.vue";
@@ -16,6 +17,7 @@ import Footer from "./components/template/Footer.vue";
 export default {
   name: "App",
   components: { Header, Menu, Content, Footer },
+  computed: mapState(["isMenuVisible"]),
 };
 </script>
 
@@ -38,5 +40,12 @@ body {
     "header header"
     "menu content"
     "menu footer";
+}
+
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
 }
 </style>

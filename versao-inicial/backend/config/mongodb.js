ccae536ff8f097026723d58f9537f5775807a339
Paulo Henrique <phttm0501@gmail.com>
2023-11-22 21:28:59 -0400

    mongodb,stat and routes

---
+++ versao-inicial/backend/config/mongodb.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/knowledge_stats', { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Conectado ao MongoDB');
+  })
+  .catch(e => {
+    const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
+    console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
+  })
+

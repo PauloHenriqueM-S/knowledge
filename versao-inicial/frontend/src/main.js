6541847d3013e3126d56938955169956037bdd5c
Paulo Henrique <phttm0501@gmail.com>
2023-11-23 20:20:26 -0400

    stats component

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -10,6 +10,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6Ikpvw6NvIiwiZW1haWwiOiJqb2FvQHlhaG9vLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNzAwNzgyMDQ5LCJleHAiOjE3MDEwNDEyNDl9.CfiGlmWhbE6uqq3E4iI5neZVXbzMgdmF7JgdgbA0NKs'
+
 new Vue({
   store,
   router,

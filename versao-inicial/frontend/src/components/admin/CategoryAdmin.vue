25a1a2fb60329337e0d7fdc107fc9d9ec24508c6
Paulo Henrique <phttm0501@gmail.com>
2023-11-27 01:30:18 -0400

    Category admin component finished

--- versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
+++ versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
@@ -1,87 +1,51 @@
 <template>
   <div class="category-admin">
     <b-form>
-      <input id="user-id" type="hidden" v-model="user.id" />
-      <b-row>
-        <b-col md="6" sm="12">
-          <b-form-group label="Nome:" label-for="user-name">
-            <b-form-input
-              id="user-name"
-              type="text"
-              v-model="user.name"
-              required
-              :readonly="mode === 'remove'"
-              placeholder="Informe o Nome do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-        <b-col md="6" sm="12">
-          <b-form-group label="E-mail:" label-for="user-email">
-            <b-form-input
-              id="user-email"
-              type="text"
-              v-model="user.email"
-              required
-              :readonly="mode === 'remove'"
-              placeholder="Informe o E-mail do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <b-form-checkbox
-        id="user-admin"
-        v-show="mode === 'save'"
-        v-model="user.admin"
-        class="mt-3 mb-3"
+      <input id="category-id" type="hidden" v-model="category.id" />
+      <b-form-group label="Nome:" label-for="category-name">
+        <b-form-input
+          id="category-name"
+          type="text"
+          v-model="category.name"
+          required
+          :readonly="mode === 'remove'"
+          placeholder="Informe o Nome da Categoria..."
+        />
+      </b-form-group>
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+        <b-form-select
+          v-if="mode === 'save'"
+          id="category-parentId"
+          :options="categories"
+          v-model="category.parentId"
+        />
+        <b-form-input
+          v-else
+          id="category-parentId"
+          type="text"
+          v-model="category.path"
+          readonly
+        />
+      </b-form-group>
+      <b-button variant="primary" v-if="mode === 'save'" @click="save"
+        >Salvar</b-button
       >
-        Administrador?
-      </b-form-checkbox>
-      <b-row v-show="mode === 'save'">
-        <b-col md="6" sm="12">
-          <b-form-group label="Senha:" label-for="user-password">
-            <b-form-input
-              id="user-password"
-              type="password"
-              v-model="user.password"
-              required
-              placeholder="Informe a Senha do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-        <b-col md="6" sm="12">
-          <b-form-group
-            label="Confirmação de Senha:"
-            label-for="user-confirm-password"
-          >
-            <b-form-input
-              id="user-confirm-password"
-              type="password"
-              v-model="user.confirmPassword"
-              required
-              placeholder="Confirme a Senha do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col xs="12">
-          <b-button variant="primary" v-if="mode === 'save'" @click="save"
-            >Salvar</b-button
-          >
-          <b-button variant="danger" v-if="mode === 'remove'" @click="remove"
-            >Excluir</b-button
-          >
-          <b-button class="ml-2" @click="reset">Cancelar</b-button>
-        </b-col>
-      </b-row>
+      <b-button variant="danger" v-if="mode === 'remove'" @click="remove"
+        >Excluir</b-button
+      >
+      <b-button class="ml-2" @click="reset">Cancelar</b-button>
     </b-form>
     <hr />
-    <b-table hover striped :items="users" :fields="fields">
+    <b-table hover striped :items="categories" :fields="fields">
       <template v-slot:cell(actions)="data">
-        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+        <b-button
+          variant="warning"
+          @click="loadCategory(data.item)"
+          class="mr-2"
+        >
           <i class="fa fa-pencil"></i>
         </b-button>
-        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+        <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
           <i class="fa fa-trash"></i>
         </b-button>
       </template>
@@ -90,8 +54,67 @@
 </template>
 
 <script>
+import { baseApiUrl, showError } from "@/global";
+import axios from "axios";
+
 export default {
   name: "CategoryAdmin",
+  data: function () {
+    return {
+      mode: "save",
+      category: {},
+      categories: [],
+      fields: [
+        { key: "id", label: "Código", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "path", label: "Caminho", sortable: true },
+        { key: "actions", label: "Ações" },
+      ],
+    };
+  },
+  methods: {
+    loadCategories() {
+      const url = `${baseApiUrl}/categories`;
+      axios.get(url).then((res) => {
+        // this.categories = res.data;
+        this.categories = res.data.map((category) => {
+          return { ...category, value: category.id, text: category.path };
+        });
+      });
+    },
+    reset() {
+      this.mode = "save";
+      this.category = {};
+      this.loadCategories();
+    },
+    save() {
+      const method = this.category.id ? "put" : "post";
+      const id = this.category.id ? `/${this.category.id}` : "";
+      axios[method](`${baseApiUrl}/categories${id}`, this.category)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    remove() {
+      const id = this.category.id;
+      axios
+        .delete(`${baseApiUrl}/categories/${id}`)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    loadCategory(category, mode = "save") {
+      this.mode = mode;
+      this.category = { ...category };
+    },
+  },
+  mounted() {
+    this.loadCategories();
+  },
 };
 </script>
 

2341849e9c4bf4b07002b3cb136cddc319bb2ef1
Paulo Henrique <phttm0501@gmail.com>
2023-11-27 03:17:06 -0400

    Article admin finished

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -12,7 +12,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZGVyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDA4MDM4OTAsImV4cCI6MTcwMTA2MzA5MH0.YzNWp9pAY0ZpX0abR2vuVd2ZQdSRq_m6cjHoeoMUBwM'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZGVyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDEwNjY2MTIsImV4cCI6MTcwMTMyNTgxMn0.dFLLO5LKUM_COiMV6kJDDU2YGtoMpGfK16xrr3UpC5U'
 
 new Vue({
   store,
